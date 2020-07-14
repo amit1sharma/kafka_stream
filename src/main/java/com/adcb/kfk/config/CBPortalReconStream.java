466914cd80ccdfe2c96ece45bce1cb1c640e9859
amit1sharma <funkiamit@gmail.com>
2020-07-14 14:48:16 +0400

    unused removed

--- src/main/java/com/adcb/kfk/config/CBPortalReconStream.java
+++ src/main/java/com/adcb/kfk/config/CBPortalReconStream.java
@@ -2,28 +2,29 @@ package com.adcb.kfk.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.adcb")
 @SpringBootApplication
 @EnableScheduling
-@EnableWebMvc
-public class CBPortalReconStream extends SpringBootServletInitializer{
+//@EnableWebMvc
+public class CBPortalReconStream /*extends SpringBootServletInitializer*/{
 	public static final void main(String args[]){
 		SpringApplication.run(CBPortalReconStream.class, args);
 	}
-	@Override
+
+/*	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CBPortalReconStream.class);
-	}
+	}*/
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
